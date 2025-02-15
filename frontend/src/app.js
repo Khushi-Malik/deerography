fd44c76aee2a6805b471ea6dd36c4b2a0bb8fbcb
Khushi-Malik <khushimalik1910@gmail.com>
2025-02-15 16:14:51 -0500

    fixed react

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,19 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";   // Home Page with API data fetching
-import Games from "./pages/Connections"; // Games Page
-import Navbar from "./components/Navbar"; // Navigation Bar
+import logo from './logo.PNG';
+import './App.css';
 
-const App = () => {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/games" element={<Games />} />
-            </Routes>
-        </Router>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
